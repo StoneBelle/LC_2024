bd9a45948c516bb0ac4cbede241fce6c0aab8db4
Gemm <65476500+StoneBelle@users.noreply.github.com>
2024-10-05 00:43:13 -0400

    Update 2491_Divide_Players_Into_Teams_of_Equal_Skill.py

--- Daily/Medium/2491_Divide_Players_Into_Teams_of_Equal_Skill.py
+++ Daily/Medium/2491_Divide_Players_Into_Teams_of_Equal_Skill.py
@@ -1,28 +1,29 @@
+import math 
+from collections import Counter
 class Solution:
-  def dividePlayers(self, skill: List[int]) -> int:
-      
-      # Check if given list has length of two 
-      length = len(skill)
-      pairs = int(length / 2)
+    def dividePlayers(self, skill: List[int]) -> int:
 
-      if length == 2:
-          return skill[0] * skill[1]
+        length = len(skill) # Length of given array 
+        pairs = int(length / 2) # Total number of teams 
 
-      # Divide the players into teams of even skill 
-      ordered_skills = sorted(skill)
-      first_half = ordered_skills[0:3]
-      second_half = ordered_skills[length-1:-(length-pairs + 1):-1]
+        # Check if given array has only 1 team
+        if length == 2:
+            print( skill[0] * skill[1])
 
-      teams = tuple(zip(first_half, second_half)) 
+        # Sort given array and create teams of even skill
+        ordered_skills = sorted(skill) 
+        first_half = ordered_skills[0:3] # Slices front half of array 
+        second_half = ordered_skills[length-1:-(length-pairs + 1):-1] # Slices end half of array, reverse order
+        teams = tuple(zip(first_half, second_half)) # Pairs players into even teams
 
-      chem = 0
-      x = []
-      for team in teams:
-          x.append(math.prod(team))
-          chem += math.prod(team)
-      
-      y = Counter(x)
-      if len(y) > 1 :
-          return -1
-      else:
-          return chem
+        chem = 0 # Stores chemistry sum
+        x = [] # Store the sum of each team's skill
+        for team in teams:
+            x.append(sum(team))
+            chem += math.prod(team) # Updates cumulative team chemistry
+
+        y = Counter(x) # Counts the freq of each team sum
+        if len(y) > 1:
+            return -1
+        else:
+            return chem
